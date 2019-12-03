9f285f93bb6db0a0b8905d38d95ed6539947bd2c
Sasa Pecic <s.pecic@levi9.com>
2019-12-03 11:38:18 +0100

    simple product calculation

--- test-spec.js
+++ test-spec.js
@@ -1,9 +1,24 @@
 describe('Test section 1', () => {
-  it('Test case 1', () => {
-    expect(0).toBe(1)
-  })
+  
+  it('Validate proper product', () => {
+
+    browser.get('http://juliemr.github.io/protractor-demo/');
+
+    let firstNumber = element(by.model('first'));
+    let secondNumber = element(by.model('second'));
+    let goButton = element(by.id('gobutton'));
+
+    firstNumber.sendKeys(5);
+    secondNumber.sendKeys(10);
+    goButton.click();
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+    expect(element(by.binding('latest')).getText()).not.toBeNull();
+    expect(element(by.binding('latest')).getText('15'));
+
+  });
 
   it('Test case 2', () => {
-    expect(1).toBeGreaterThan(0)
+    expect(1).toBeGreaterThan(0);
   })
-})
+});
