93a030f05d2440e00198f770a3415550f8226031
Zeljko Simic <z.simic@levi9.com>
2019-12-04 14:15:44 +0100

    Added ExpectedConditions tests

--- conf.js
+++ conf.js
@@ -1,11 +1,15 @@
+const { SpecReporter } = require('jasmine-spec-reporter')
+
 exports.config = {
   directConnect: true,
 
   specs: [
-    './test-spec.js'
+    // './test-spec.js',
+    './ec-spec.js'
   ],
 
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    jasmine.getEnv().addReporter(new SpecReporter())
   }
 }
---
+++ ec-spec.js
@@ -0,0 +1,21 @@
+const EC = protractor.ExpectedConditions
+
+describe('Expected Conditions', () => {
+
+  it('Example 1', () => {
+    browser.get('https://demo.nopcommerce.com/')
+    const computersButton = element.all(by.css('[class="top-menu notmobile"] li')).first()
+    browser.wait(EC.elementToBeClickable(computersButton), 10000)
+    computersButton.click()
+    browser.sleep(3000)
+  })
+
+  it('Example 2', () => {
+    browser.get('https://demo.nopcommerce.com/apple-macbook-pro-13-inch')
+    const cartButton = element(by.css('[class="bar-notification success"]'))
+    const addToCart = element(by.id('add-to-cart-button-4'))
+    addToCart.click()
+    browser.wait(EC.visibilityOf(cartButton))
+    cartButton.click()
+  })
+})
