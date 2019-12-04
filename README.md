8c106f794ee63f35b16ffd82ed3084d61d01e3cd
Sasa Pecic <s.pecic@levi9.com>
2019-12-04 08:29:24 +0100

    add readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # QA-praksa
+```
+
+1. Overview
+2. Environment
+3. Running
+
+```
+
+# 1. Overview
+...
+
+# 2. Environment
+---
+
+# 2. Runnning
+script
--- test-spec.js
+++ test-spec.js
@@ -1,20 +1,23 @@
 describe('Test section 1', () => {
+  let firstNumber = element(by.model('first'));
+  let secondNumber = element(by.model('second'));
+  let goButton = element(by.id('gobutton'));
+  let latestResult = element(by.binding('latest'));
   
-  it('Validate proper product', () => {
-
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-
-    let firstNumber = element(by.model('first'));
-    let secondNumber = element(by.model('second'));
-    let goButton = element(by.id('gobutton'));
+  });
+  
+  it('Validate proper addition', () => {
 
     firstNumber.sendKeys(5);
     secondNumber.sendKeys(10);
+
     goButton.click();
 
     expect(browser.getTitle()).toEqual('Super Calculator');
-    expect(element(by.binding('latest')).getText()).not.toBeNull();
-    expect(element(by.binding('latest')).getText('15'));
+    expect(latestResult.getText()).not.toBeNull();
+    expect(latestResult.getText()).toEqual('15');
 
   });
 
